3c5f4e78cb33e5344c16f0e6a588a203207abdbc
Myasnikov Leo <levandreevich95@gmail.com>
2023-08-02 20:20:24 +0300

    Не удается активировать класс TestBase в другом проекте

--- Adressbook-web-tests/Adressbook-web-tests/tests/GroupTests.cs
+++ Adressbook-web-tests/Adressbook-web-tests/tests/GroupTests.cs
@@ -25,9 +25,23 @@ namespace Adressbook_web_tests
             return groups;
         }
 
+        public static IEnumerable<GroupData> GroupDataFromFile() 
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+            }
+            return groups;
+        }
 
-
-        [Test, TestCaseSource("RandomGroupdataProvider")]
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
 
