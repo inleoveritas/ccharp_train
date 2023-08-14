a53b69f1a9a386acb91bd06450ea2aab8c65d692
Myasnikov Leo <levandreevich95@gmail.com>
2023-08-14 20:28:27 +0300

    Создание группы по заданию 16

---
+++ addressbook_tests_autoit/Appmanager/GroupHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        //задаем переменную, чтобы каждый раз название окна не писать
+        public static string GROUPWINTITLE = "Group editor";
+
+        public GroupHelper(ApplicationManager manager) : base(manager) 
+        {
+            
+        }
+
+        public void Add(GroupData newGroup)
+        {
+
+            OpenGroupsDialogue();
+            //Нажимаем на кнопку добавления новой группы
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            //Вводим текст (название группы)
+            aux.Send(newGroup.Name);
+            //Эмулируем нажатие клавиши "Enter"
+            aux.Send("{ENTER}");
+            //Нажимаем на кнопку закрытия окна групп
+            CloseGroupsDialogue();
+
+        }
+
+        private void CloseGroupsDialogue()
+        {
+            //Нажимаем на кнопку закрытия окна групп
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
+        }
+
+        private void OpenGroupsDialogue()
+        {
+            //Название окна, текст кнопки, идентификатор
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
+            //Ждем открытия окна с группами
+            aux.WinWait(GROUPWINTITLE);
+        }
+
+        //Получаем список
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();   
+            OpenGroupsDialogue();
+            //Используем метод для работы с элементами SysTreeView
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetItemCount", "#0", "");
+           for (int i = 0; i <int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetText", "#0|#"+i, "");
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            }
+            CloseGroupsDialogue();
+            return list;
+        }
+    }
+}
---
+++ addressbook_tests_autoit/Tests/GroupCreationTest.cs
@@ -0,0 +1,31 @@
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTest : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            //Запомнили старый список
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "test"
+            };
+
+            app.Groups.Add(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();   
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
